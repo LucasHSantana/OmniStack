a9e15861c8849b8c88b05cfd77c0de2f668da172
Lucas Henrique Santana <lukaz.santana@gmail.com>
2019-08-30 20:37:03 -0300

    Versão 1

---
+++ frontend/build/precache-manifest.19cb3532bb85844972673140e28ae6a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1743a26c378e19860ab1de8953d004e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "273b19f803ec9da40bf2",
+    "url": "/static/css/main.b99448fe.chunk.css"
+  },
+  {
+    "revision": "7ee645b3ac88680f5457",
+    "url": "/static/js/2.9c2f01ba.chunk.js"
+  },
+  {
+    "revision": "273b19f803ec9da40bf2",
+    "url": "/static/js/main.d3ba8189.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b066eb4b8770b84fda6dceded2612d64",
+    "url": "/static/media/dislike.b066eb4b.svg"
+  },
+  {
+    "revision": "21c10b2613fbc1b8b36859e433819ed1",
+    "url": "/static/media/itsamatch.21c10b26.png"
+  },
+  {
+    "revision": "739b1b166578175a083d2c4e8bbb0312",
+    "url": "/static/media/like.739b1b16.svg"
+  },
+  {
+    "revision": "b37460d1d458a66bf64597a8169e3bcb",
+    "url": "/static/media/logo.b37460d1.svg"
+  }
+]);
